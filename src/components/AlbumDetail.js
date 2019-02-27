476e7688843d6b21549701a0a001f5509546977f
PatAkil <patrickakil@hotmail.com>
2019-02-27 08:57:44 +0100

    Made the app scrollable and added a button component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Image, Text, View } from 'react-native';
+import { Image, Text, View, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   const { headerContentStyle, headerTextStyle, thumbnailStyle, thumbnailContainerStyle, imageContainerStyle } = styles;
   return (
     <Card>
@@ -24,6 +25,11 @@ const AlbumDetail = ({ album }) => {
         <Image 
         style={imageContainerStyle}
         source={{ uri: image }} />
+      </CardSection>
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}>
+          Buy now
+        </Button>
       </CardSection>
     </Card>
   );
